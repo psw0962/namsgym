fe48402a94fa182b5a45453607493079f250262a
psw0962 <psw0962@naver.com>
2024-05-29 17:25:01 +0900

    working

--- components/resetting-map-bounds.jsx
+++ components/resetting-map-bounds.jsx
@@ -35,48 +35,33 @@
 
 // export default ReSetttingMapBounds;
 
-import React, { useEffect, useMemo } from 'react';
+import { useEffect, useMemo } from 'react';
 import { useMap } from 'react-kakao-maps-sdk';
 
-const ReSetttingMapBounds = ({ points, isSingle = false }) => {
+const ReSettingMapBounds = ({ points, isSingle = false }) => {
   const map = useMap();
 
   const bounds = useMemo(() => {
-    if (typeof window === 'undefined' || typeof window.kakao === 'undefined') {
-      return null;
-    }
-
     const bounds = new kakao.maps.LatLngBounds();
-
     if (isSingle) {
-      bounds.extend(new window.kakao.maps.LatLng(points?.lat, points?.lng));
+      if (points) {
+        bounds.extend(new kakao.maps.LatLng(points.lat, points.lng));
+      }
     } else {
       points?.forEach(point => {
-        bounds.extend(new window.kakao.maps.LatLng(point.lat, point.lng));
+        bounds.extend(new kakao.maps.LatLng(point.lat, point.lng));
       });
     }
-
     return bounds;
   }, [points, isSingle]);
 
   useEffect(() => {
-    if (
-      typeof window === 'undefined' ||
-      typeof window.kakao === 'undefined' ||
-      !bounds
-    ) {
-      console.error('Kakao Maps API is not loaded or bounds is not defined');
-      return;
-    }
-
-    if (isSingle) {
-      map.setBounds(bounds);
-    } else if (points?.length > 0) {
+    if (points && points.length > 0) {
       map.setBounds(bounds);
     }
-  }, [points, bounds, map, isSingle]);
+  }, [points, bounds, map]);
 
   return null;
 };
 
-export default ReSetttingMapBounds;
+export default ReSettingMapBounds;
