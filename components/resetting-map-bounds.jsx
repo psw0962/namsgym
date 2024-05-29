c1cdc8bac23b6a683a47d6e326e78c1a4b5bb2d1
psw0962 <psw0962@naver.com>
2024-05-29 17:13:37 +0900

    working

--- components/resetting-map-bounds.jsx
+++ components/resetting-map-bounds.jsx
@@ -1,34 +1,74 @@
+// import { useEffect, useMemo } from 'react';
+// import { useMap } from 'react-kakao-maps-sdk';
+
+// const ReSetttingMapBounds = ({ points, isSingle = false }) => {
+//   const map = useMap();
+//   const bounds = useMemo(() => {
+//     const bounds = new kakao.maps.LatLngBounds();
+
+//     if (isSingle) {
+//       bounds.extend(new kakao.maps.LatLng(points?.lat, points?.lng));
+
+//       return bounds;
+//     } else {
+//       points?.forEach(point => {
+//         bounds.extend(new kakao.maps.LatLng(point.lat, point.lng));
+//       });
+//       return bounds;
+//     }
+//   }, [points]);
+
+//   useEffect(() => {
+//     if (isSingle) {
+//       map.setBounds(bounds);
+//       return;
+//     }
+
+//     if (isSingle === false && points?.length > 0) {
+//       map.setBounds(bounds);
+//       return;
+//     }
+//   }, [points, bounds, map]);
+
+//   return null;
+// };
+
+// export default ReSetttingMapBounds;
+
 import { useEffect, useMemo } from 'react';
 import { useMap } from 'react-kakao-maps-sdk';
 
 const ReSetttingMapBounds = ({ points, isSingle = false }) => {
   const map = useMap();
+
   const bounds = useMemo(() => {
+    if (typeof kakao === 'undefined') {
+      return null;
+    }
+
     const bounds = new kakao.maps.LatLngBounds();
 
     if (isSingle) {
       bounds.extend(new kakao.maps.LatLng(points?.lat, points?.lng));
-
-      return bounds;
     } else {
       points?.forEach(point => {
         bounds.extend(new kakao.maps.LatLng(point.lat, point.lng));
       });
-      return bounds;
     }
-  }, [points]);
+
+    return bounds;
+  }, [points, isSingle]);
 
   useEffect(() => {
-    if (isSingle) {
-      map.setBounds(bounds);
+    if (typeof kakao === 'undefined' || !bounds) {
+      console.error('Kakao Maps API is not loaded or bounds is not defined');
       return;
     }
 
-    if (isSingle === false && points?.length > 0) {
+    if (isSingle || points?.length > 0) {
       map.setBounds(bounds);
-      return;
     }
-  }, [points, bounds, map]);
+  }, [points, bounds, map, isSingle]);
 
   return null;
 };
